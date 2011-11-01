241d4567230109668ad23b44d20c627541a8649b
Jerome Etienne <jerome.etienne@gmail.com>
2011-11-01 13:51:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+NoWebGL is a debug library for WebGL. 
+In short, it declares all WebGL functions as dummy.
+Its purpose is to rule out WebGL performance when you optimize, and thus see more clearly the effect of your javascript.
+
+This is directly inpired from [firebugx](http://code.google.com/p/fbug/source/browse/branches/firebug1.2/lite/firebugx.js)
+
+**Note** This is super early work. pull request are welcomed
---
+++ nowebgl.js
@@ -0,0 +1,83 @@
+var names = [
+	"log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+	"group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"
+];
+
+(function(){
+	
+})()
+var NoWebGL	= {};
+window.NoWebGL	= NoWebGL;
+
+NoWebGL.Context	= {}
+
+NoWebGL.Fct	= {}
+NoWebGL.Fct.returnUndefined	= function(){ return undefined;	}
+NoWebGL.Fct.returnTrue		= function(){ return true;	}
+NoWebGL.Fct.returnObject	= function(){ return {};	}
+
+NoWebGL.Fct.undefinedList	= 
+[
+'enable',
+'clearColor',
+'depthFunc',
+'blendEquationSeparate',
+'blendFuncSeparate',
+'viewport',
+'clear',
+
+'deleteTexture',
+
+'bindTexture',
+'activeTexture',
+
+'texImage2D',
+'texParameteri',
+'uniform1i',
+
+
+'bindBuffer',
+'bufferData',
+'vertexAttribPointer',
+'drawElements',
+
+'shaderSource',
+'compileShader',
+'attachShader',
+'linkProgram',
+'useProgram',
+'enableVertexAttribArray'
+];
+
+NoWebGL.Fct.trueList	= [
+	'getShaderParameter',
+	'getProgramParameter',
+];
+NoWebGL.Fct.objList	= [
+	'createShader',
+	'createProgram',
+	'createTexture',
+	'createBuffer',
+	'getAttribLocation',
+	'getUniformLocation',
+];
+
+// list of constant that need to be set
+NoWebGL.Fct.constantList	= [
+	'FRAGMENT_SHADER',
+	'VERTEX_SHADER',
+];
+
+NoWebGL.Fct.constantList.forEach(function(method){
+	NoWebGL.Context[method]	= 1;
+})
+NoWebGL.Fct.undefinedList.forEach(function(method){
+	NoWebGL.Context[method]	= NoWebGL.Fct.returnUndefined;
+})
+NoWebGL.Fct.trueList.forEach(function(method){
+	NoWebGL.Context[method]	= NoWebGL.Fct.returnTrue;
+})
+NoWebGL.Fct.objList.forEach(function(method){
+	NoWebGL.Context[method]	= NoWebGL.Fct.returnObject;
+})
+
