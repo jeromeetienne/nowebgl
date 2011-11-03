fdd268d2017a7fa4b66c02cb260993fb9cb04a30
Jerome Etienne <jerome.etienne@gmail.com>
2011-11-03 08:01:12 +0100

    updated nowebgl.js + better README + Makefile + learningwebgl lesson01 as example

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-NoWebGL is a debug library for WebGL. 
-In short, it declares all WebGL functions as dummy.
-Its purpose is to rule out WebGL performance when you optimize, and thus see more clearly the effect of your javascript.
+NoWebGL is a library to help benchmarking your WebGL demo.
+In short, it declares all WebGL functions as dummy,
+so you can measure more accuratly the time taken by the rest e.g. javascripot.
 
-This is directly inpired from [firebugx](http://code.google.com/p/fbug/source/browse/branches/firebug1.2/lite/firebugx.js)
+Its purpose is to rule out WebGL performance when you optimize, and thus
+see more clearly the effect of your javascript.
 
-instead of the usual
+This isnt foolproof, mainly because the dummy functions are real dummy.
+If your code is doing a lot of ```gl.get*``` and is expecting real results, it may cause trouble.
+This is directly inpired from [firebugx](http://code.google.com/p/fbug/source/browse/branches/firebug1.2/lite/firebugx.js).
+This is early work. suggestions and/or pull request are welcomed.
+
+## Let's include it
+
+```
+    <script src="nowebgl.js"></script>
+```
+
+## Let's use it
+
+It is quite simple really. Instead of the usual
 
 ```
     var gl = domElement.getContext('experimental-webgl');
 ```
 
-Do the following
+Just do the following
 
 ```
     var gl = NoWebGL.getContext();
 ```
-**Note** This is super early work. pull request are welcomed
--- nowebgl.js
+++ nowebgl.js
@@ -3,45 +3,56 @@
 var NoWebGL	= {};
 global.NoWebGL	= NoWebGL;
 
-NoWebGL.Context	= {}
+//////////////////////////////////////////////////////////////////////////////////
+//		define all the functions to emulate				//
+//////////////////////////////////////////////////////////////////////////////////
+NoWebGL.Fct			= {}
 
-NoWebGL.Fct	= {}
+// list of functions returning undefined
 NoWebGL.Fct.undefinedList	= [
 	'enable',
+	'finish',
+	'viewport',
+	'clear',
+	'save',
+	'restore',
+
 	'clearColor',
 	'depthFunc',
 	'blendEquationSeparate',
 	'blendFuncSeparate',
-	'viewport',
-	'clear',
-	
-	'deleteTexture',
 	
 	'bindTexture',
 	'activeTexture',
-	
+	'deleteTexture',
 	'texImage2D',
 	'texParameteri',
+
 	'uniform1i',
-	
+	'uniformMatrix4fv',
 	
 	'bindBuffer',
 	'bufferData',
 	'vertexAttribPointer',
 	'drawElements',
+	'drawArrays',
 	
 	'shaderSource',
 	'compileShader',
 	'attachShader',
 	'linkProgram',
 	'useProgram',
-	'enableVertexAttribArray'
+
+	'enableVertexAttribArray',
 ];
 
+// list of function returning true
 NoWebGL.Fct.trueList	= [
 	'getShaderParameter',
 	'getProgramParameter',
 ];
+
+// list of function returning {}
 NoWebGL.Fct.objList	= [
 	'createShader',
 	'createProgram',
@@ -57,12 +68,16 @@ NoWebGL.Fct.constantList	= [
 	'VERTEX_SHADER',
 ];
 
+// define the various functions
 NoWebGL.Fct.returnUndefined	= function(){ return undefined;	}
 NoWebGL.Fct.returnTrue		= function(){ return true;	}
 NoWebGL.Fct.returnObject	= function(){ return {};	}
 
+NoWebGL.Context		= {};
+NoWebGL.getContext	= function(){	return NoWebGL.Context;	}
+
 NoWebGL.Fct.constantList.forEach(function(method){
-	NoWebGL.Context[method]	= 1;
+	NoWebGL.Context[method]	= 1;	// each constant is equal to 1
 });
 NoWebGL.Fct.undefinedList.forEach(function(method){
 	NoWebGL.Context[method]	= NoWebGL.Fct.returnUndefined;
